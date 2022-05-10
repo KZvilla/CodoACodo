e4a73de7d5a20cb2b7456f8fe3f5b979d2ab8241
KZvilla <40326801+KZvilla@users.noreply.github.com>
2022-05-10 15:06:46 -0300

    tarea

--- Tareas/tablamulti.js
+++ Tareas/tablamulti.js
@@ -1,4 +1,6 @@
-let num = Number(prompt("Ingrese un numero que desee una tabla del 0 al 10"))
+let num = Number(prompt("Ingrese un numero que desee una tabla del 0 al 10"));
 for (let index = 0; index < 10; index++) {
-    console.log(num*i)
+  document.write(num + " x " + index + " = " + num * index);
+  document.write("<br>");
+  console.log(num*index)
 }
