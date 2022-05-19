11bab86b09fd922d6221dd56c1b74cf70dfa4467
KZvilla <40326801+KZvilla@users.noreply.github.com>
2022-05-19 15:38:11 -0300

    Clase 21

---
+++ Clases/ClaseDom.js
@@ -0,0 +1,8 @@
+//Veanmos los objetos del DOM
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.all);//Esto significa deprecado
+// console.log(document.getElementById('titulo').textContent="Este es un titulo") // getElementById tiene sus propias metodos(muchas veces heredadas)
+
+var tituloPagina = document.getElementById("titulo");
+var subtitulo = document.getElementById("subtitulo");
